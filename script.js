e8584cd58376dcc6cb2948221871bc5fa8e33f5f
King William <seanapowi@gmail.com>
2025-06-24 12:24:33 +0300

    Feat: Coding Challenge one

--- script.js
+++ script.js
@@ -44,16 +44,45 @@ const dog2 = new Dog('Luna', 'Husky');
 dog2.bark();
  */
 
-class Dog {
-  constructor(name, breed) {
-    this.name = name;
-    this.breed = breed;
-  }
+/* Your tasks:
 
-  bark() {
-    console.log(`${this.name} says, Woof Woof!!`);
-  }
-}
+Use a constructor function to implement a 'Car'. A car has a 'make' and a
+'speed' property. The 'speed' property is the current speed of the car in
+km/h
 
-const myDog = new Dog('Mufasa', 'German Shepherd');
-myDog.bark();
+Implement an 'accelerate' method that will increase the car's speed by 10,
+and log the new speed to the console
+
+Implement a 'brake' method that will decrease the car's speed by 5, and log
+the new speed to the console
+
+Create 2 'Car' objects and experiment with calling 'accelerate' and
+'brake' multiple times on each of them
+Test data:
+§ Data car 1: 'BMW' going at 120 km/h
+§ Data car 2: 'Mercedes' going at 95 km/h
+GOOD LUCK 😀 */
+
+/* const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed}`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed}`);
+};
+
+const myCar = new Car('BMW', 120);
+const myCar1 = new Car('Mercedes', 95);
+
+myCar.accelerate();
+myCar1.accelerate();
+myCar.brake();
+myCar1.brake(); */
