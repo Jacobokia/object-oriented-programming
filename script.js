75c00a647b3ae8267245c08ea21ec8eac7a0ffbd
Jacobokia <cobs399@gmail.com>
2025-06-30 12:09:25 +0300

    feat:Inheritance and polymorphism solution

--- script.js
+++ script.js
@@ -109,31 +109,30 @@ GOOD LUCK 😀
 */
 
 //create class
+/*
 class carCl {
-  constructor (make, speed) {
-    this.make = make,
-    this.speed = speed
+  constructor(make, speed) {
+    (this.make = make), (this.speed = speed);
   }
-//method to accelerate
-   accelerate() {
-    this.speed += 10
-    console.log(`${this.make} is going at ${this.speed} in km/h`)
-   }
-//method to brake
-   brake() {
-    this.speed -= 5
-    console.log(`${this.make} is going at ${this.speed} in km/h`)
-   }
-//getter for kmh to miles conversion
-  get speedUS () {
-    return this.speed / 1.6
+  //method to accelerate
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} in km/h`);
   }
-//Setter for miles to kmh conversion
-  set speedUS (speedinMiles) {
-    this.speed = speedinMiles * 1.6
-  } 
-
-};
+  //method to brake
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} in km/h`);
+  }
+  //getter for kmh to miles conversion
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+  //Setter for miles to kmh conversion
+  set speedUS(speedinMiles) {
+    this.speed = speedinMiles * 1.6;
+  }
+}
 
 const ford = new carCl('Ford', 120);
 
@@ -141,9 +140,78 @@ ford.accelerate();
 ford.brake();
 
 //Using Getter
-console.log(`${ford.make} is going at ${ford.speedUS} miles per hour`)
+console.log(`${ford.make} is going at ${ford.speedUS} miles per hour`);
 
 //Using Setter
-ford.speedUS = 80
-console.log(`${ford.make} is now going at ${ford.speed} Km per hour and ${ford.speedUS} miles per hour`)
+ford.speedUS = 80;
+console.log(
+  `${ford.make} is now going at ${ford.speed} Km per hour and ${ford.speedUS} miles per hour`
+);
+*/
+/*
 
+Coding Challenge #3
+Your tasks:
+
+1.Use a constructor function to implement an Electric Car (called 'EV') as a child
+"class" of 'Car'. Besides a make and current speed, the 'EV' also has the
+current battery charge in % ('charge' property)
+
+2.Implement a 'chargeBattery' method which takes an argument
+'chargeTo' and sets the battery charge to 'chargeTo'
+
+3.Implement an 'accelerate' method that will increase the car's speed by 20,
+and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140
+km/h, with a charge of 22%'
+
+4.Create an electric car object and experiment with calling 'accelerate',
+'brake' and 'chargeBattery' (charge to 90%). Notice what happens when
+you 'accelerate'! Hint: Review the definiton of polymorphism 😉
+Test data:
+§ Data car 1: 'Tesla' going at 120 km/h, with a charge of 23%
+GOOD LUCK 😀
+
+*/
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed}`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed}`);
+};
+
+//constructor function for a EV
+function EV(make, speed, charge) {
+  Car.call(this, make, speed); //Calling a parent constructor
+  this.charge = charge; // Adding a child property
+}
+
+// Inherit the prototypes
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} is going at ${this.speed} km/h with a ${this.charge}%`
+  );
+};
+
+const tesla = new EV('Tesla', 120, 23);
+
+tesla.accelerate();
+tesla.brake();
+tesla.chargeBattery(90);
+tesla.accelerate();
